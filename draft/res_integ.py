4e5d372747ddfd9525c6669584d07c7e565ab76d
Christopher Albert <albert@alumni.tugraz.at>
2020-10-26 11:55:19 +0100

    Method of the Romanians

--- draft/res_integ.py
+++ draft/res_integ.py
@@ -8,6 +8,7 @@
 #%%
 from numpy import *
 from matplotlib.pyplot import *
+from plotly.express import *
 
 def f(x, y): return x**2 + y**2 - 1.0
 def gradf(x, y): return array((2.0*x, 2.0*y))
@@ -185,7 +186,7 @@ nu = 1
 nv = 1
 
 w = empty((nu+1, nv+1, 3))
-w[0, 0, :] = (1 .0, 0.0, 0.0)
+w[0, 0, :] = (1.0, 0.0, 0.0)
 
 for ku in range(nu):
     for kv in range(nv):
@@ -243,7 +244,6 @@ for kv in range(nv):
         w[ku+1, kv, :2] = sol.x
         H[ku+1] = H[ku] + du  # To check if we integrate arc length correctly
 
-    from plotly.express import *
 
 scatter_3d(
     x = w[:,:,0].flatten(),
@@ -267,7 +267,7 @@ def rootfun(w, wprev):
 
 ku = 1
 w2 = empty((nv+1, 3))
-w2[0, :] = np.append(wmid[ku], x30)
+w2[0, :] = wmid[ku,:]
 
 for kv in range(nv):
     sol = root(
@@ -298,7 +298,92 @@ Figure(
     )
 )
 
+#%% Step one: find curve on surface by cutting with coordinate plane
+
+du = 2*pi/40
+dv = 2*pi/40
+nu = 20
+nv = 10
+
+w = zeros((nu+1, nv+1, 3))
+
+x30 = 0.0  # initial z coordinate
+#w[0, :, :] = (sqrt(1.0 - x30**2), 0.0, x30)
+w[0, :, :] = (0.0, sqrt(1.0 - x30**2), x30)
+
+#w[0, 0, :] = (1.0, 0.0, 0.0)
+
+def rootfun(w, wprev):
+    wmid = (w + wprev[0:2])/2.0
+    df = gradf(wmid[0], wmid[1], wprev[2])
+    jac = sqrt(sum(df**2))
+    return np.array((
+        w[0] - wprev[0] + du*df[1]/jac,
+        w[1] - wprev[1] - du*df[0]/jac
+    ))
 
 
+def rootfun2(w, wprev):
+    wmid = (w + wprev[[2,0]])/2.0
+    df = gradf(wmid[1], wprev[1], wmid[0])
+    jac = sqrt(sum(df**2))
+    return np.array((
+        w[0] - wprev[2] + dv*df[0]/jac,
+        w[1] - wprev[0] - dv*df[2]/jac
+    ))
 
+H = empty((nu+1))
+H = empty((nu+1, nv+1))
+H[0] = 0
+H2 = 0.0  # for surface integral
+
+for kv in range(nv):
+    sol = root(
+        rootfun2,
+        x0=w[0, kv, [2,0]],
+        args=w[0, kv, :], tol=1e-13
+    )
+    w[0, kv+1, 1] = w[0, kv, 1]
+    w[0, kv+1, [2,0]] = sol.x
+
+for ku in range(nu):
+    sol = root(
+        rootfun,
+        x0=w[ku, 0, :2],
+        args=w[ku, 0, :], tol=1e-13
+    )
+    #print(sol.success)
+    w[ku+1, 0, 2] = w[ku, 0, 2]
+    w[ku+1, 0, :2] = sol.x
+    H[ku+1] = H[ku] + du  # To check if we integrate arc length correctly
+
+    for kv in range(nv):
+      sol = root(
+          rootfun2,
+          x0=w[ku+1, kv, [2,0]],
+          args=w[ku+1, kv, :], tol=1e-13
+      )
+      w[ku+1, kv+1, 1] = w[ku+1, kv, 1]
+      w[ku+1, kv+1, [2,0]] = sol.x
+
+      A = w[ku, kv, :]
+      B = w[ku+1, kv, :]
+      C = w[ku+1, kv+1, :]
+      D = w[ku, kv+1, :]
+      a2 = sum((B-A)**2)
+      b2 = sum((C-B)**2)
+      c2 = sum((D-C)**2)
+      d2 = sum((A-D)**2)
+      p2 = sum((A-C)**2)
+      q2 = sum((B-D)**2)
+      dS = 0.25*sqrt(4.0*p2*q2 - (a2 + c2 - b2 - d2)**2)  # Bretschneider's formula
+      H2 = H2+dS
+
+print('Error: {} %'.format((4.0 - H2/pi)*100.0))
+
+scatter_3d(
+    x = w[:,:,0].flatten(),
+    y = w[:,:,1].flatten(),
+    z = w[:,:,2].flatten()
+)
 # %%
