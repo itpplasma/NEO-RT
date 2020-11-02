d8b1e8a8cccb1770dde0f8dd881876ea1f92190b
Christopher Albert <albert@alumni.tugraz.at>
2020-11-02 16:50:56 +0100

    Line endings, bounce

--- VER_2/SRC/orbit_full.f90
+++ VER_2/SRC/orbit_full.f90
@@ -13,23 +13,23 @@ module orbit
     call velo(tau, z, vz)
   end subroutine timestep
 
-  subroutine bounce_average(n, z, integrand, out)
+  subroutine bounce_average(n, z, integrand, res)
   ! Returns bounce average of an quantity via orbit integration
     integer(4), intent(in) :: n  ! Number of components of the integrand
     real(8), intent(in)    :: z(neqm)    ! Starting position
     external               :: integrand  ! Subroutine
-    real(8), intent(out)   :: out(n)     ! Bounce average output
+    real(8), intent(out)   :: res(n)     ! Bounce average output
 
     real(8) :: dtau, taub, delphi
 
     dtau = 2.0d0  ! TODO: time step is for strongly passing
 
-    out = 0.0d0
+    res = 0.0d0
 
-    call find_bounce(n, timestep_ext, dtau, z, taub, delphi, out)
-    out = out/taub
+    call find_bounce(n, timestep_ext, dtau, z, taub, delphi, res)
+    res = res/taub
     print *, 'Out: '
-    print *, out
+    print *, res
 
     contains
 
@@ -48,16 +48,16 @@ module orbit
 
   end subroutine bounce_average
 
-  subroutine bounce_harmonic(next, z, mb, nph, out, fn)
+  subroutine bounce_harmonic(next, z, mb, nph, res, fn)
     ! Computes bounce harmonic mb of fn
       integer(4), intent(in) :: next       ! Number of extra integrals
       real(8), intent(inout) :: z(5)       ! Position on orbit
       integer(4), intent(in) :: mb         ! Bounce harmonic number
       integer(4), intent(in) :: nph        ! Toroidal harmonic number
-      real(8), intent(out)   :: out(next)  ! Complex harmonic of input fn
+      real(8), intent(out)   :: res(next)  ! Complex harmonic of input fn
       external               :: fn         ! Subroutine fn(z, out) to treat
 
-      call bounce_average(2, z, fn, out)
+      call bounce_average(2, z, fn, res)
 
     end subroutine bounce_harmonic
 end module orbit
