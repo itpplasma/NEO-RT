f29514a385343245de149f75a392a5784b06b783
Christopher Albert <albert@alumni.tugraz.at>
2020-11-02 10:10:10 +0100

    Line feed

--- POTATO/VER_2/RUN/ttt.bat
+++ POTATO/VER_2/RUN/ttt.bat
@@ -1,1 +1,4 @@
+#!/bin/sh
+
 gfortran --check=all SRC/alpha_lifetime_mod.f90 SRC/chamb_divB0.f90 SRC/magfie_cyl.f90 SRC/period_mod.f90 SRC/input_files.f90 SRC/field_c_mod.f90 SRC/field_mod.f90 SRC/inthecore_mod.f90 SRC/field_eq_mod.f90 SRC/field_divB0.f90 SRC/bdivfree_mod.f90 SRC/amn_mod.f90 SRC/theta_rz_mod.f90 SRC/extract_fluxcoord_mod.f90 SRC/bdivfree.f90 SRC/spline5_RZ.f90 SRC/sub_alpha_lifetime.f90 SRC/odeint_allroutines.f SRC/collis_nbi.f90 SRC/rng.f90 SRC/plag_coeff.f90 SRC/sub_potato.f90 SRC/level_set.f90 SRC/minpack.f90 SRC/plot_orbit_set.f90 SRC/sample_orbits.f90 SRC/tt.f90 -llapack
+
