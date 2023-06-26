f40310f5a05f90bc215b8756c4891dc23a5c1591
Christopher Albert <albert@alumni.tugraz.at>
2023-06-26 11:07:59 +0200

    Ripple plateau test cleaned

--- test/ripple_plateau/test_ripple_plateau.py
+++ test/ripple_plateau/test_ripple_plateau.py
@@ -1,7 +1,7 @@
 import os
 import numpy as np
 
-os.system('rm *.out')
+os.system('rm -f *.out')
 os.system('../../BUILD/neo_rt.x driftorbit_test')
 
 #%%
@@ -11,17 +11,18 @@ data = np.loadtxt('driftorbit_test.out')
 D11 = data[4]
 D12 = data[8]
 
-print(f'D11/epsmn**2 = {D11/epsmn**2}')
-print(f'D12/epsmn**2 = {D12/epsmn**2}')
-print(f'D12/D11 = {D12/D11}')
-
 with open('driftorbit_test_magfie_param.out', 'r') as f:
     for line in f:
         if 'Drp' in line:
             Drp = float(line.split()[-1])
             break
 
-print(f'Drp = {Drp}')
+print(f'NEO-RT:    D11/eps**2   = {D11/epsmn**2:.3e}')
+print(f'Reference: D11rp/eps**2 = {Drp:.3e}')
+print()
+print(f'NEO-RT:    D12/D11     = {D12/D11:.3e}')
+print(f'Reference: D12rp/D11rp = {3.0:.3e}')
+print()
 
 assert(abs(D12/D11 - 3.0) < 0.15)
 assert(abs((D11/epsmn**2 - Drp)/Drp) < 0.05)
