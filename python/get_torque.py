2c50f2b58fb19c70a9edc963031c7d93940765c9
Christopher Albert <albert@alumni.tugraz.at>
2024-04-19 15:13:59 +0000

    Correct torque density

--- python/get_torque.py
+++ python/get_torque.py
@@ -1,13 +1,11 @@
 def get_torque(data):
     NM_IN_DYNCM = 1e-7
-    M3_IN_CM3 = 1e-6
+    CM3_IN_M3 = 1e-6
 
-    s = data[0]
     dVds = data[1]
-    M_t = data[2]
     Tco = data[3]
     Tctr = data[4]
     Tt = data[5]
     Ttot = Tco + Tctr + Tt  # Torque density in CGS over s
-    Tphi_dens = Ttot*NM_IN_DYNCM*dVds*M3_IN_CM3  # Torque density in SI
+    Tphi_dens = (Ttot*NM_IN_DYNCM)/(dVds*CM3_IN_M3)  # Torque density in SI
     return Tphi_dens
