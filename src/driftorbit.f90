4c8ef895b38f0956a6fd78a30645a409565a642a
Christopher Albert <albert@alumni.tugraz.at>
2025-02-19 17:39:04 +0100

    wip: test omega prime

--- src/driftorbit.f90
+++ src/driftorbit.f90
@@ -876,14 +876,11 @@ contains
         real(8) :: D11int
         real(8) :: ux, etax
         real(8) :: Omth, dOmthdv, dOmthdeta
-        real(8) :: dummy, dummy2
-        real(8) :: OmtB
         real(8) :: Hmn2
 
         v = ux*vth
         eta = etax
         call Om_th(Omth, dOmthdv, dOmthdeta)
-        call Om_tB(OmtB, dummy, dummy2)
         taub = 2d0*pi/abs(Omth)
         call bounce_fast
         Hmn2 = (bounceavg(4)**2 + bounceavg(5)**2)*(mi*(ux*vth)**2/2d0)**2
@@ -898,14 +895,11 @@ contains
         real(8) :: D12int
         real(8) :: ux, etax
         real(8) :: Omth, dOmthdv, dOmthdeta
-        real(8) :: dummy, dummy2
-        real(8) :: OmtB
         real(8) :: Hmn2
 
         v = ux*vth
         eta = etax
         call Om_th(Omth, dOmthdv, dOmthdeta)
-        call Om_tB(OmtB, dummy, dummy2)
         taub = 2d0*pi/abs(Omth)
         call bounce_fast
         Hmn2 = (bounceavg(4)**2 + bounceavg(5)**2)*(mi*(ux*vth)**2/2d0)**2
@@ -933,32 +927,33 @@ contains
         dOmdv = mth*dOmthdv + mph*dOmphdv
         dOmdeta = mth*dOmthdeta + mph*dOmphdeta
         dOmdpph = -(qi/c*iota*psi_pr)**(-1)*(mth*dOmthds + mph*dOmphds)
-        Ompr = mth*(eta*dOmdeta - ux*vth/2*dOmdv)/(mi*(ux*vth)**2/(2d0*Omth)) + dOmdpph
+        Ompr = omega_prime(ux, Omth, dOmdv, dOmdeta, dOmdpph)
         call coleff(ux, dpp, dhh, fpeff)
         dhh = vth*dhh
         dpp = vth**3*dpp
         dres = dpp*(dOmdv/Ompr)**2 + dhh*eta*(bounceavg(6) - eta)*(dOmdeta/Ompr)**2
         dnorm = dres*sqrt(abs(Ompr))/sqrt(abs(Hmn2))**(3d0/2d0)
         call attenuation_factor(dnorm, nonlinear_attenuation)
-    end function
+    end function nonlinear_attenuation
+
+    function omega_prime(ux, Omth, dOmdv, dOmdeta, dOmdpph)
+        real(8), intent(in) :: ux, Omth, dOmdv, dOmdeta, dOmdpph
+        real(8) :: omega_prime
+        omega_prime = mth*(eta*dOmdeta - ux*vth/2*dOmdv)/(mi*(ux*vth)**2/(2d0*Omth)) + dOmdpph
+    end function omega_prime
 
     function Tphi_int(ux, etax)
         real(8) :: Tphi_int
         real(8) :: ux, etax
         real(8) :: Omth, dOmthdv, dOmthdeta
-        real(8) :: dummy, dummy2
-        real(8) :: OmtB
         real(8) :: Hmn2
         real(8) :: dpp, dhh, fpeff, dres, dnorm, thatt, & ! for nonlin
                    Omph, dOmphdv, dOmphdeta, dOmdv, dOmdeta, Ompr, dOmphds, dOmthds, &
                    dOmdpph
-        real(8) :: ma, mb, mc, md, me, mf
-        real(8) :: dvdJ, detadJ
 
         v = ux*vth
         eta = etax
         call Om_th(Omth, dOmthdv, dOmthdeta)
-        call Om_tB(OmtB, dummy, dummy2)
         taub = 2d0*pi/abs(Omth)
         call bounce_fast
         Hmn2 = (bounceavg(4)**2 + bounceavg(5)**2)*(mi*(ux*vth)**2/2d0)**2
@@ -971,19 +966,7 @@ contains
             dOmdeta = mth*dOmthdeta + mph*dOmphdeta
             dOmdpph = -(qi/c*iota*psi_pr)**(-1)*(mth*dOmthds + mph*dOmphds)
 
-            ma = mi*(ux*vth)*mi*c/qi*eta
-            mb = mi*(ux*vth)**2/2*mi*c/qi
-            mc = mi/(2d0*Omth)*(ux*vth)*(1d0 - eta*bounceavg(7))
-            md = mi*(ux*vth)**2/2d0*Omth
-            me = -mth/mph
-            mf = 1d0/mph
-
-            dvdJ = mb*me/(ma*md*mf - mb*mc*mf)
-            detadJ = ma*me/(mb*mc*mf - ma*md*mf)
-
-            !Ompr=mth*(eta*dOmdeta-ux*vth/2*dOmdv)/(mi*(ux*vth)**2/(2d0*Omth))+dOmdpph
-
-            Ompr = dOmdv*dvdJ + dOmdeta*detadJ + mph*dOmdpph
+            Ompr = omega_prime_new(ux, Omth, dOmdv, dOmdeta, dOmdpph)
 
             if (intoutput) then
                 ! 0:n, 1:l, 2:Eth, 3:Jperp_tp, 4:drphi/dpphi, 5:E/Eth, 6:Jperp/Jperp_tp, 7:rphi,
@@ -1006,6 +989,25 @@ contains
 
         Tphi_int = -pi**(3d0/2d0)*mph**2*ni1*c*vth/qi*ux**3*exp(-ux**2)*taub*Hmn2*thatt*(A1 + A2*ux**2)
     end function Tphi_int
+
+    function omega_prime_new(ux, Omth, dOmdv, dOmdeta, dOmdpph)
+        real(8), intent(in) :: ux, Omth, dOmdv, dOmdeta, dOmdpph
+        real(8) :: omega_prime_new
+
+        real(8) :: ma, mb, mc, md, me, mf, dvdJ, detadJ
+
+        ma = mi*(ux*vth)*mi*c/qi*eta
+        mb = mi*(ux*vth)**2/2*mi*c/qi
+        mc = mi/(2d0*Omth)*(ux*vth)*(1d0 - eta*bounceavg(7))
+        md = mi*(ux*vth)**2/2d0*Omth
+        me = -mth/mph
+        mf = 1d0/mph
+
+        dvdJ = mb*me/(ma*md*mf - mb*mc*mf)
+        detadJ = ma*me/(mb*mc*mf - ma*md*mf)
+
+        omega_prime_new = dOmdv*dvdJ + dOmdeta*detadJ + mph*dOmdpph
+    end function omega_prime_new
 
     subroutine taurel
         ! test box counting
---
+++ test/test_omega_prime.f90
@@ -0,0 +1,66 @@
+program main
+    use neort, only: read_control, check_magfie, init_profile, init_plasma, init_run, &
+                     compute_transport_harmonic, runname, s, M_t
+    use driftorbit
+    implicit none
+
+    character(1024) :: TEST_RUN = "driftorbit64.new"
+
+    runname = TEST_RUN
+    call read_control
+    call do_magfie_init
+    call init_profile
+    call init_plasma
+    call init_run(use_thermodynamic_profiles=.true.)
+    call check_magfie
+
+    mth = -3
+    sigv = 1
+    etamin = (1 + epst)*etatp
+    etamax = (1 - epst)*etadt
+
+    call test_omega_prime
+
+contains
+
+    subroutine test_omega_prime
+        real(8) :: ux
+        real(8) :: Om, dOmdv, dOmdeta, Ompr_old, Ompr_new, dOmdpph, Omth, dOmthdeta
+        real(8) :: roots(nlev, 3), eta_res(2)
+        integer :: nroots
+
+        ux = 0.9d0
+        v = ux*vth
+
+        call driftorbit_coarse(etamin, etamax, roots, nroots)
+        eta_res = driftorbit_root(1d-8*abs(Om_tE), roots(1, 1), roots(1, 2))
+
+        call compute_frequencies(ux, eta_res, Omth, Om, dOmdv, dOmdeta, dOmdpph)
+        Ompr_old = omega_prime(ux, Omth, dOmdv, dOmdeta, dOmdpph)
+        Ompr_new = omega_prime_new(ux, Omth, dOmdv, dOmdeta, dOmdpph)
+
+        print *, s, v, eta, Om, Ompr_old, Ompr_new
+
+    end subroutine test_omega_prime
+
+    subroutine compute_frequencies(ux, etax, Omth, Om, dOmdv, dOmdeta, dOmdpph)
+        real(8), intent(in) :: ux, etax(2)
+        real(8), intent(out) :: Omth, Om, dOmdv, dOmdeta, dOmdpph
+
+        real(8) :: Omph, dOmphdv, dOmphdeta, dOmphds, dOmthds, dOmthdv, dOmthdeta
+
+        v = ux*vth
+        eta = etax(1)
+
+        call Om_th(Omth, dOmthdv, dOmthdeta)
+        taub = 2d0*pi/abs(Omth)
+        call bounce_fast
+        call Om_ph(Omph, dOmphdv, dOmphdeta)
+        call d_Om_ds(dOmthds, dOmphds)
+        Om = mth*Omth + mph*Omph
+        dOmdv = mth*dOmthdv + mph*dOmphdv
+        dOmdeta = mth*dOmthdeta + mph*dOmphdeta
+        dOmdpph = -(qi/c*iota*psi_pr)**(-1)*(mth*dOmthds + mph*dOmphds)
+    end subroutine compute_frequencies
+
+end program main
