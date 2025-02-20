eef3831e7598fb48567cc46270cda0cbb4747041
Christopher Albert <albert@tugraz.at>
2025-02-20 22:16:34 +0100

    tmp

--- test/test_omega_prime.f90
+++ test/test_omega_prime.f90
@@ -62,7 +62,7 @@ contains
         character(1024) :: temp_file
         integer :: funit, i
 
-        temp_file = "/dev/shm/test_omega_prime.dat"
+        temp_file = "/tmp/test_omega_prime.dat"
 
         print *, "Writing results to ", trim(adjustl(temp_file))
 
