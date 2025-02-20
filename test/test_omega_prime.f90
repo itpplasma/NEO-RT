853e97128e17a784d83c10b27ffeade295be7d0f
Christopher Albert <albert@tugraz.at>
2025-02-20 22:15:53 +0100

    omegaprime test

--- test/test_omega_prime.f90
+++ test/test_omega_prime.f90
@@ -1,28 +1,81 @@
-program main
+program test_omage_prime_prog
     use neort, only: read_control, check_magfie, init_profile, init_plasma, init_run, &
                      compute_transport_harmonic, runname, s, M_t
     use driftorbit
     implicit none
 
-    character(1024) :: TEST_RUN = "driftorbit64.new"
-    real(8) :: ds = 0.001d0
+    type :: freq_data_t
+        real(8) :: s, u, eta
+        real(8) :: J(3), Jbar(3), Om, Ompr_old, Ompr_new
+    end type
 
-    runname = TEST_RUN
-
-    call read_control
-
-    call setup
-    call test_omega_prime
-
-    s = s + 0.5d0*ds
-    call setup
-    call test_omega_prime
-
-    s = s - ds
-    call setup
-    call test_omega_prime
-
+    call main
 contains
+
+    subroutine main
+        integer, parameter :: NUM_SAMPLES = 16
+        character(1024), parameter :: TEST_RUN = "driftorbit64.new"
+        real(8) :: s0, ux0, eta0
+        real(8) :: delta = 1d-10
+        real(8) :: eta_res(2)
+
+        type(freq_data_t) :: freq_data(NUM_SAMPLES)
+
+        runname = TEST_RUN
+
+        call read_control
+        call setup
+        s0 = s
+        ux0 = v/vth
+        eta_res = first_resonance()
+        eta0 = eta_res(1)
+
+        call sample_values(s0, ux0, eta0, delta, freq_data)
+        call show_results(freq_data)
+    end subroutine main
+
+    subroutine sample_values(s0, ux0, eta0, delta, freq_data)
+        real(8), intent(in) :: s0, ux0, eta0, delta
+        type(freq_data_t), intent(out) :: freq_data(:)
+
+        integer :: i
+        real(8) :: xi
+
+        freq_data(1)%s = s0
+        freq_data(1)%u = ux0
+        freq_data(1)%eta = eta0
+        call test_omega_prime(freq_data(1))
+        do i = 2, size(freq_data)
+            call random_number(xi)
+            freq_data(i)%s = s0*(1d0 + delta*(xi-0.5d0))
+            call random_number(xi)
+            freq_data(i)%u = ux0*(1d0 + delta*(xi-0.5d0))
+            call random_number(xi)
+            freq_data(i)%eta = eta0*(1d0 + delta*(xi-0.5d0))
+            call test_omega_prime(freq_data(i))
+        end do
+    end subroutine sample_values
+
+    subroutine show_results(freq_data)
+        type(freq_data_t), intent(in) :: freq_data(:)
+
+        character(1024) :: temp_file
+        integer :: funit, i
+
+        temp_file = "/dev/shm/test_omega_prime.dat"
+
+        print *, "Writing results to ", trim(adjustl(temp_file))
+
+        open(newunit=funit, file=temp_file, status='unknown')
+        write(funit, *) "# s, u, eta, J(1), J(2), J(3), Jbar(1), Jbar(2), Jbar(3), Om, Ompr_old, Ompr_new"
+
+        do i = 1, size(freq_data)
+            write(funit, *) freq_data(i)
+        end do
+
+        close(funit)
+
+    end subroutine show_results
 
     subroutine setup
         call do_magfie_init
@@ -37,34 +90,37 @@ contains
         etamax = (1 - epst)*etadt
     end subroutine setup
 
-    subroutine test_omega_prime
-        real(8) :: ux
-        real(8) :: Om, dOmdv, dOmdeta, Ompr_old, Ompr_new, dOmdpph, Omth, dOmthdeta
-        real(8) :: roots(nlev, 3), eta_res(2)
+    function first_resonance() result(eta_res)
+        real(8) :: eta_res(2)
+        real(8) :: roots(nlev, 3)
         integer :: nroots
-
-        ux = 0.9d0
-        v = ux*vth
-
         call driftorbit_coarse(etamin, etamax, roots, nroots)
         eta_res = driftorbit_root(1d-8*abs(Om_tE), roots(1, 1), roots(1, 2))
+    end function first_resonance
 
-        call compute_frequencies(ux, eta_res, Omth, Om, dOmdv, dOmdeta, dOmdpph)
-        Ompr_old = omega_prime(ux, Omth, dOmdv, dOmdeta, dOmdpph)
-        Ompr_new = omega_prime_new(ux, Omth, dOmdv, dOmdeta, dOmdpph)
+    subroutine test_omega_prime(freq_data)
+        type(freq_data_t), intent(inout) :: freq_data
+        real(8) :: dOmdv, dOmdeta, Ompr_old, Ompr_new, dOmdpph, Omth, dOmthdeta, vpar
 
-        print *, s, v, eta, Om, Ompr_old, Ompr_new
+        s = freq_data%s
+        call setup
 
+        freq_data%J(1) = Jperp()
+        ! TODO: J(2) = Jpar, J(3) = pphi
+
+        call compute_frequencies(freq_data%u, freq_data%eta, Omth, freq_data%Om, dOmdv, dOmdeta, dOmdpph)
+        freq_data%Ompr_old = omega_prime(freq_data%u, Omth, dOmdv, dOmdeta, dOmdpph)
+        freq_data%Ompr_new = omega_prime_new(freq_data%u, Omth, dOmdv, dOmdeta, dOmdpph)
     end subroutine test_omega_prime
 
     subroutine compute_frequencies(ux, etax, Omth, Om, dOmdv, dOmdeta, dOmdpph)
-        real(8), intent(in) :: ux, etax(2)
+        real(8), intent(in) :: ux, etax
         real(8), intent(out) :: Omth, Om, dOmdv, dOmdeta, dOmdpph
 
         real(8) :: Omph, dOmphdv, dOmphdeta, dOmphds, dOmthds, dOmthdv, dOmthdeta
 
         v = ux*vth
-        eta = etax(1)
+        eta = etax
 
         call Om_th(Omth, dOmthdv, dOmthdeta)
         taub = 2d0*pi/abs(Omth)
@@ -77,4 +133,4 @@ contains
         dOmdpph = -(qi/c*iota*psi_pr)**(-1)*(mth*dOmthds + mph*dOmphds)
     end subroutine compute_frequencies
 
-end program main
+end program test_omage_prime_prog
