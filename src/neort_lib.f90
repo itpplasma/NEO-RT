b859ea165a93dc151513f6d2dd34d509603d09a4
Andreas Zach <andreas.zach@student.tugraz.at>
2026-01-04 14:05:24 +0100

    Clean interface to call neo-rt as library

---
+++ src/neort_lib.f90
@@ -0,0 +1,108 @@
+module neort_lib
+    use iso_fortran_env, only: dp => real64
+    use neort_datatypes, only: transport_data_t
+
+    implicit none
+
+    private
+    public :: neort_init
+    public :: neort_prepare_splines
+    public :: neort_prepare_splines_from_files
+    public :: neort_compute_at_s
+    public :: neort_deinit
+
+contains
+
+    subroutine neort_init(config_file, boozer_file, boozer_pert_file)
+        use neort, only: read_and_set_control
+        use do_magfie_mod, only: read_boozer_file
+        use do_magfie_pert_mod, only: read_boozer_pert_file
+        use driftorbit, only: pertfile, nonlin
+        use thetadata_mod, only: init_attenuation_data
+
+        character(len=*), intent(in) :: config_file
+        character(len=*), intent(in) :: boozer_file
+        character(len=*), intent(in), optional :: boozer_pert_file
+
+        call read_and_set_control(config_file)
+        call read_boozer_file(boozer_file)
+
+        if (present(boozer_pert_file) .and. pertfile) then
+            call read_boozer_pert_file(boozer_pert_file)
+        end if
+
+        if (nonlin) call init_attenuation_data()
+    end subroutine neort_init
+
+    subroutine neort_prepare_splines(nplasma, am1, am2, Z1, Z2, plasma_data, profile_data)
+        use neort_profiles, only: prepare_plasma_splines, prepare_profile_splines
+
+        integer, intent(in) :: nplasma
+        real(dp), intent(in) :: am1, am2, Z1, Z2
+        real(dp), intent(in) :: plasma_data(:, :)
+        real(dp), intent(in) :: profile_data(:, :)
+
+        call prepare_plasma_splines(nplasma, am1, am2, Z1, Z2, plasma_data)
+        call prepare_profile_splines(profile_data)
+    end subroutine neort_prepare_splines
+
+    subroutine neort_prepare_splines_from_files(plasma_file, profile_file)
+        use neort_profiles, only: read_plasma_input, prepare_plasma_splines, prepare_profile_splines
+        use util, only: readdata
+
+        character(len=*), intent(in) :: plasma_file
+        character(len=*), intent(in) :: profile_file
+
+        integer :: nplasma
+        real(dp) :: am1, am2, Z1, Z2
+        real(dp), allocatable :: plasma_data(:, :)
+        real(dp), allocatable :: profile_data(:, :)
+
+        call read_plasma_input(plasma_file, nplasma, am1, am2, Z1, Z2, plasma_data)
+        call prepare_plasma_splines(nplasma, am1, am2, Z1, Z2, plasma_data)
+        deallocate (plasma_data)
+
+        call readdata(profile_file, 2, profile_data)
+        call prepare_profile_splines(profile_data)
+        deallocate (profile_data)
+    end subroutine neort_prepare_splines_from_files
+
+    subroutine neort_compute_at_s(s_val, transport_data_out)
+        use neort, only: set_s, compute_transport, set_to_trapped_region
+        use do_magfie_mod, only: init_magfie_at_s, R0, psi_pr, q
+        use do_magfie_pert_mod, only: init_magfie_pert_at_s, init_mph_from_shared
+        use driftorbit, only: pertfile, nopassing, sign_vpar, etamin, etamax, efac, bfac, comptorque
+        use neort_profiles, only: init_plasma_at_s, init_profile_at_s, init_thermodynamic_forces
+        use neort_magfie, only: init_flux_surface_average
+        use neort_freq, only: init_canon_freq_trapped_spline, init_canon_freq_passing_spline
+
+        real(dp), intent(in) :: s_val
+        type(transport_data_t), intent(out) :: transport_data_out
+
+        call set_s(s_val)
+
+        call init_magfie_at_s()
+        if (pertfile) call init_magfie_pert_at_s()
+        call init_mph_from_shared()
+
+        call init_plasma_at_s()
+        call init_profile_at_s(R0, efac, bfac)
+
+        call init_flux_surface_average(s_val)
+        call init_canon_freq_trapped_spline()
+        if (.not. nopassing) call init_canon_freq_passing_spline()
+        sign_vpar = 1
+        call set_to_trapped_region(etamin, etamax)
+        if (comptorque) call init_thermodynamic_forces(psi_pr, q)
+
+        call compute_transport(transport_data_out)
+    end subroutine neort_compute_at_s
+
+    subroutine neort_deinit()
+        use neort_profiles, only: plasma_spl_coeff, Mt_spl_coeff
+
+        if (allocated(plasma_spl_coeff)) deallocate (plasma_spl_coeff)
+        if (allocated(Mt_spl_coeff)) deallocate (Mt_spl_coeff)
+    end subroutine neort_deinit
+
+end module neort_lib
