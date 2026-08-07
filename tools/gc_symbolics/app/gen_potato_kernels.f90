3b45ed95aef3743af3907c95cf9c477055ded05f
Christopher Albert <albert@tugraz.at>
2026-08-07 22:34:57 +0200

    Separate POTATO envelope kernel input

--- tools/gc_symbolics/app/gen_potato_kernels.f90
+++ tools/gc_symbolics/app/gen_potato_kernels.f90
@@ -48,7 +48,7 @@ program gen_potato_kernels
 
     type(expr_t) :: mode_m, mode_n, delta_phi
     type(expr_t) :: harmonic_target, harmonic_extent, resonance_g
-    type(expr_t) :: no_root_margin, current_extent_envelope
+    type(expr_t) :: no_root_margin, current_extent_envelope, harmonic_extent_input
     type(expr_t) :: extent_envelope
 
     type(expr_t) :: orbit_H_m_squared, maxwellian_weight, Phi_eff
@@ -160,7 +160,9 @@ program gen_potato_kernels
     resonance_g = delta_phi + 2*pi_expr(arena)*mode_m/mode_n
     no_root_margin = abs(delta_phi) - harmonic_extent
     current_extent_envelope = sym(arena, 'current_extent_envelope')
-    extent_envelope = func('max', [current_extent_envelope, harmonic_extent])
+    harmonic_extent_input = sym(arena, 'harmonic_extent')
+    extent_envelope = func('max', &
+        [current_extent_envelope, harmonic_extent_input])
 
     ! ------------------------------------------------------------------
     ! Eq. (17) delta-root and torque weight.  The negative pi^(3/2)/4
@@ -290,7 +292,8 @@ program gen_potato_kernels
         no_root_margin - (abs(delta_phi) - harmonic_extent))
     call check_identity(proofs, proof_engine, &
         'finite harmonic extent envelope', &
-        extent_envelope - func('max', [current_extent_envelope, harmonic_extent]))
+        extent_envelope - &
+        func('max', [current_extent_envelope, harmonic_extent_input]))
     call check_identity(proofs, proof_engine, &
         'delta-root weight uses absolute mode number', &
         delta_root_weight - abs(mode_n)*root_jacobian)
