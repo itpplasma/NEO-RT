3edb44c3e6a20a4e2a7f2b2b57dcbcc1a68bf029
Christopher Albert <albert@tugraz.at>
2026-08-07 14:02:16 +0200

    Isolate POTATO pitch boundary test linkage

--- POTATO/SRC/sub_potato.f90
+++ POTATO/SRC/sub_potato.f90
@@ -3,29 +3,6 @@
 ! Modules:
 !ccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
 !
-  module pitch_boundary_mod
-    implicit none
-    integer, parameter :: pitch_boundary_success=0
-    integer, parameter :: pitch_boundary_forbidden=2
-  contains
-    pure subroutine resolve_pitch_squared(ratio, pitch_squared, ierr)
-      double precision, intent(in) :: ratio
-      double precision, intent(out) :: pitch_squared
-      integer, intent(out) :: ierr
-      double precision, parameter :: relative_tolerance=1.d-9
-      double precision :: tolerance
-
-      pitch_squared=1.d0-ratio
-      tolerance=relative_tolerance*max(1.d0,abs(ratio))
-      if(pitch_squared.lt.-tolerance) then
-        ierr=pitch_boundary_forbidden
-        return
-      endif
-      pitch_squared=max(0.d0,pitch_squared)
-      ierr=pitch_boundary_success
-    end subroutine resolve_pitch_squared
-  end module pitch_boundary_mod
-!
   module phielec_of_psi_mod
     integer, parameter :: npolyphi=9
     double precision, dimension(0:npolyphi) :: polyphi
