7003fea3e721b1918ecf682f45dfd4e502a9fab0
Christopher Albert <albert@tugraz.at>
2026-08-08 02:19:02 +0200

    Regenerate EQDSK cut flux curvature kernels

--- src/generated/neort_generated_certificate_registry.f90
+++ src/generated/neort_generated_certificate_registry.f90
@@ -4,15 +4,18 @@ module neort_generated_certificate_registry
         'fortsym@545788453a204d58705f735b519c3863c2f734c8'
     character(*), parameter :: regenerate_command = &
         'cd tools/gc_symbolics && fo exec gen_full_fow_physics ../../src/generated'
-    integer, parameter :: certificate_count = 20
+    integer, parameter :: certificate_count = 23
     character(len=32), parameter :: certificate_id(certificate_count) = &
         [character(len=32) :: 'geometry', 'littlejohn', 'eq13_cdot', 'boundary_limits', &
         'root_enclosures', 'interpolation', 'profile_endpoints', &
         'refinement', 'harmonic_integrand', 'simple_root_force', &
-        'eqdsk_cell_jet', 'eqdsk_profile_jet', 'eqdsk_cut_jet', &
+        'eqdsk_cell_jet', 'eqdsk_cell_fourth_jet', &
+        'eqdsk_profile_jet', 'eqdsk_cut_jet', &
         'eqdsk_cut_numerator_jet', 'eqdsk_cut_r_chart', &
         'eqdsk_cut_z_chart', 'eqdsk_cut_r_flux_chart', &
-        'eqdsk_cut_mean_value', 'eqdsk_cut_axis_curvature', &
+        'eqdsk_cut_numerator_hessian', &
+        'eqdsk_cut_r_flux_curvature', 'eqdsk_cut_mean_value', &
+        'eqdsk_cut_axis_curvature', &
         'eqdsk_cut_axis_limit' ]
     character(len=64), parameter :: certificate_fingerprint(certificate_count) = &
         [character(len=64) :: 'neort-cert-v1:geometry:19:fortsym-5457884', &
@@ -26,12 +29,15 @@ module neort_generated_certificate_registry
         'neort-cert-v1:harmonic_integrand:8:fortsym-5457884', &
         'neort-cert-v1:simple_root_force:3:fortsym-5457884', &
         'neort-cert-v1:eqdsk_cell_jet:10:fortsym-5457884', &
+        'neort-cert-v1:eqdsk_cell_fourth_jet:5:fortsym-5457884', &
         'neort-cert-v1:eqdsk_profile_jet:4:fortsym-5457884', &
         'neort-cert-v1:eqdsk_cut_jet:7:fortsym-5457884', &
         'neort-cert-v1:eqdsk_cut_numerator_jet:3:fortsym-5457884', &
         'neort-cert-v1:eqdsk_cut_r_chart:2:fortsym-5457884', &
         'neort-cert-v1:eqdsk_cut_z_chart:2:fortsym-5457884', &
         'neort-cert-v1:eqdsk_cut_r_flux_chart:2:fortsym-5457884', &
+        'neort-cert-v1:eqdsk_cut_numerator_hessian:3:fortsym-5457884', &
+        'neort-cert-v1:eqdsk_cut_r_flux_curvature:4:fortsym-5457884', &
         'neort-cert-v1:eqdsk_cut_mean_value:1:fortsym-5457884', &
         'neort-cert-v1:eqdsk_cut_axis_curvature:2:fortsym-5457884', &
         'neort-cert-v1:eqdsk_cut_axis_limit:3:fortsym-5457884' ]
