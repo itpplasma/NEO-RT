27c14ca322108671f30b69bd683a650bc4f9879a
Christopher Albert <albert@tugraz.at>
2026-08-09 08:56:29 +0200

    Revert "Keep class sampling off rho boundary endpoints"
    This reverts commit a479c2007a0a4fa7f6b28403d4ef69d7514f71f8.

--- POTATO/SRC/sub_potato.f90
+++ POTATO/SRC/sub_potato.f90
@@ -2623,24 +2623,24 @@
   select case(ifuntype)
   case(11)
 ! two rho_pol boundaries, 0<x<1
-    xbeg=relmargin
-    xend=1.d0-relmargin
+    xbeg=0.d0
+    xend=1.d0
   case(12)
 ! left- rho_pol boundary, right - inner boundary, 0<x<1
-    xbeg=relmargin
+    xbeg=0.d0
     xend=1.d0-turn_margin
   case(13)
 ! left- rho_pol boundary, right - X-point, 0<x<inf
-    xbeg=atanh(relmargin)
+    xbeg=0.d0
     xend=-0.5d0*log(relmargin/widthclass)
   case(14)
 ! left- rho_pol boundary, right - X-point, 0<x<inf
-    xbeg=atanh(relmargin)
+    xbeg=0.d0
     xend=-0.5d0*log(relmargin/widthclass)*0.5d0
   case(21)
 ! left- inner boundary, right - rho_pol boundary, 0<x<1
     xbeg=turn_margin
-    xend=sqrt(1.d0-relmargin)
+    xend=1.d0
   case(22)
 ! two inner boundaries, 0<x<1
     turn_x=acos(1.d0-2.d0*relmargin)/acos(-1.d0)
@@ -2657,7 +2657,7 @@
   case(31)
 ! left- X-point, right - rho_pol boundary, -inf<x<0
     xbeg=0.5d0*log(relmargin/widthclass)
-    xend=-atanh(relmargin)
+    xend=0.d0
   case(32)
 ! left- X-point, right - inner boundary, -inf<x<0
     xbeg=0.5d0*log(relmargin/widthclass)
@@ -2673,7 +2673,7 @@
   case(41)
 ! left- X-point, right - rho_pol boundary, -inf<x<0
     xbeg=0.25d0*log(relmargin/widthclass)
-    xend=-atanh(relmargin)
+    xend=0.d0
   case(42)
 ! left- X-point, right - inner boundary, -inf<x<0
     xbeg=0.25d0*log(relmargin/widthclass)
